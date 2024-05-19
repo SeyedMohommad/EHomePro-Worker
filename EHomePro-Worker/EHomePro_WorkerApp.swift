fc39ef3148312089d82c57232e1e400085b2f54f
SeyedMohammadAminAltolie-Dev <a.amin771116@yahoo.com>
2024-05-19 11:59:11 +0330

    Initial Commit

---
+++ EHomePro-Worker/EHomePro_WorkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EHomePro_WorkerApp.swift
+//  EHomePro-Worker
+//
+//  Created by Ashkan Amin on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct EHomePro_WorkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
